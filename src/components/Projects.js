81269f54ed7d26f0f217d830aa4dc30093452461
elvstejd <elvistejeda0910@gmail.com>
2021-10-05 17:27:59 -0400

    Put images in the source code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,21 @@
 import { SectionTitle } from "../styles/shared/SectionTitle";
 import ProjectCard from "./ProjectCard";
 import styled from "styled-components";
+import project1Image from '../images/pftcalc-screencap.png';
+import project2Image from '../images/timetask-screencap.png';
 
 const projects = [
     {
         name: "TimeTask",
         description: "Lorem ipsum dolor sit amet is the beggining of the most common lorem ipsum text which is a placeholder in design compositions.",
-        image: "http://localhost:3001/timetask-screencap.png",
+        image: project1Image,
         github: "https://github.com/elvstejd/timetask",
         live: "https://timetask.netlify.app/"
     },
     {
         name: "Crypto Profit Calculator",
         description: "Lorem ipsum dolor sit amet is the beggining of the most common lorem ipsum text which is a placeholder in design compositions.",
-        image: "http://localhost:3001/pftcalc-screencap.png",
+        image: project2Image,
         github: "",
         live: ""
     },
